cbe12cd225e663017449c00499edec5220f50aff
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-09-30 10:31:23 -0500

    add: First commit of the project

---
+++ src/main/java/com/ibm/aac/gerardo/api/gateway/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.ibm.aac.gerardo.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+	private Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		log.info("Path of the request received -> {}", exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+}
